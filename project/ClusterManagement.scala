6130676230c008fa1e9e515d15caaf2fadb18812
fady <i-fadyabdelaziz@menacommerce.com>
2017-05-07 17:53:05 +0200

    ADS-128: add readme and minor improvements

--- project/ClusterManagement.scala
+++ project/ClusterManagement.scala
@@ -10,7 +10,7 @@ object ClusterManagement {
 
   val appName = "ClusterManagement"
 
-  val defaultVersion = "2.0"
+  val defaultVersion = "2.1"
 
   val license = Seq("MIT" -> url("http://opensource.org/licenses/MIT"))
 
